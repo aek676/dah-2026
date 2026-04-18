5f0a377d67a1e88796c7304ef4855146607b2a52
aek676 <aek676@inlumine.ual.es>
2026-04-18 17:04:56 +0200

    feat(test): add HomePage DOM interaction tests
    Add tests for HomePage to verify DOM updates when the button is clicked
    and when the title is changed. Switch Karma to use Firefox for testing
    and add the required launcher dependency.

--- actividad5/src/app/features/home/home.page.spec.ts
+++ actividad5/src/app/features/home/home.page.spec.ts
@@ -1,18 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { HomePage } from './home.page';
+import { HomePage } from "./home.page";
+import { By } from "@angular/platform-browser";
 
-describe('HomePage', () => {
+describe("HomePage", () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
   beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HomePage],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("initialize the title from signal", () => {
+    const el: HTMLElement = fixture.nativeElement;
+    expect(el.querySelector("ion-title")?.textContent.trim()).toBe("Home Page");
+  });
+
+  it("should update the word in the DOM when button clicked", () => {
+    const button = fixture.debugElement.query(By.css("ion-button"));
+    button.triggerEventHandler("click", null);
+
+    fixture.detectChanges();
+
+    expect(component.word()).not.toBe("Hello World!");
+  });
+
+  it("should update the title in the DOM when changeTitle is called", () => {
+    component.changeTitle("New title");
+    fixture.detectChanges();
+
+    const el: HTMLElement = fixture.nativeElement;
+    expect(el.querySelector("ion-title")?.textContent).toContain("New title");
   });
 });
